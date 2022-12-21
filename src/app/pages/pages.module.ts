4b4301117ae7900d990ce93205cf19775a3b5ce8
alemcar <sandromuniz27@hotmail.com>
2022-12-21 15:09:36 -0400

    created pages home

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    ContentComponent
+    ContentComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
