c5b2524a02bc15b23ef3169eb814a2f7f5782dfd
alemcar <sandromuniz27@hotmail.com>
2022-12-21 15:57:06 -0400

    add components big-card

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { BigCardComponent } from './big-card/big-card.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './menu-title/menu-title.component';
@@ -15,7 +17,8 @@ import { SmallCardComponent } from './small-card/small-card.component';
     SmallCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
 export class ComponentsModule { }
