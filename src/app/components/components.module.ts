b1da10858f193d44de34107abab4bf65596bba6d
alemcar <sandromuniz27@hotmail.com>
2022-12-21 15:04:29 -0400

    created components menu-title

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BigCardComponent } from './big-card/big-card.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './menu-title/menu-title.component';
 
 
 
 @NgModule({
   declarations: [
     BigCardComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    MenuTitleComponent
   ],
   imports: [
     CommonModule
