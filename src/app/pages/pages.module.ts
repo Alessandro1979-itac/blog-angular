3591facbc9e9090f07764b97f9cfaec82c753b76
alemcar <sandromuniz27@hotmail.com>
2022-12-21 16:28:07 -0400

    add pages content

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { ContentComponent } from './content/content.component';
 import { HomeComponent } from './home/home.component';
 
@@ -11,7 +13,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
 export class PagesModule { }
