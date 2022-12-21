6a907db0df245131cb72e7ea9f28af84921c279e
alemcar <sandromuniz27@hotmail.com>
2022-12-21 15:03:58 -0400

    created components menu-bar

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BigCardComponent } from './big-card/big-card.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 
 
 @NgModule({
   declarations: [
-    BigCardComponent
+    BigCardComponent,
+    MenuBarComponent
   ],
   imports: [
     CommonModule
