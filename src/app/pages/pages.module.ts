f404e5ef6aeb830f099104606396caf2988a6d48
alemcar <sandromuniz27@hotmail.com>
2022-12-21 15:08:11 -0400

    created components content

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContentComponent } from './content/content.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContentComponent
+  ],
   imports: [
     CommonModule
   ]
